7d8b8af2939144290462bf76471e9d56476049bc
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-10-31 16:46:41 +0300

    Add stats on popular programming languages

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ from pprint import pprint
 
 def main():
     url = "https://api.hh.ru/vacancies"
-    params = {
-        "text": "программист",
-        "area": "1",
-        "search_period": 30
-    }
-    response = requests.get(url, params=params)
-    response.raise_for_status
-    api_response = response.json()
-    # pprint(response.url)
-    # pprint(api_response.keys())
-    pprint(api_response["found"])
+    languages = ["Java Script", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C", "Go"]
+    number_of_vacancies = {}
+    for language in languages:
+        params = {
+            "text": f"программист {language}",
+            "area": "1",
+            "search_period": ""
+        }
+        response = requests.get(url, params=params)
+        response.raise_for_status
+        api_response = response.json()
+        number_of_vacancies[language] = api_response["found"]
+    pprint(number_of_vacancies)
 
 
 if __name__ == "__main__":
