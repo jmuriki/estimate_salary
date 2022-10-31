6399076a78f403bcdc29f69649a4824b03efc47f
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-10-31 15:13:26 +0300

    Add area & search period

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from pprint import pprint
 def main():
     url = "https://api.hh.ru/vacancies"
     params = {
-        "text": "программист"
-
+        "text": "программист",
+        "area": "1",
+        "search_period": 30
     }
     response = requests.get(url, params=params)
     response.raise_for_status
-    pprint(response.json())
+    api_response = response.json()
+    # pprint(response.url)
+    # pprint(api_response.keys())
+    pprint(api_response["found"])
 
 
 if __name__ == "__main__":
