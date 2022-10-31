222fcbece4606b98b4177d694ecfd36cfcc20456
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-10-31 18:53:35 +0300

    Add predict_rub_salary and upgrade vacancies stats

--- main.py
+++ main.py
@@ -3,10 +3,33 @@ import requests
 from pprint import pprint
 
 
+def predict_rub_salary(vacancy):
+    salary = vacancy["salary"]
+    if salary and salary["currency"] == "RUR":
+        if salary["from"] and salary["to"]:
+            return (salary["from"] + salary["to"]) / 2
+        elif salary["from"] and not salary["to"]:
+            return salary["from"] * 1.2
+        elif not salary["from"] and salary["to"]:
+            return salary["to"] * 0.8
+    else:
+        return None
+
+
 def main():
     url = "https://api.hh.ru/vacancies"
-    languages = ["Java Script", "Java", "Python", "Ruby", "PHP", "C++", "C#", "C", "Go"]
-    number_of_vacancies = {}
+    languages = [
+        "Java Script",
+        "Java",
+        "Python",
+        "Ruby",
+        "PHP",
+        "C++",
+        "C#",
+        "C",
+        "Go"
+    ]
+    vacancies_stats = {}
     for language in languages:
         params = {
             "text": f"программист {language}",
@@ -16,8 +39,21 @@ def main():
         response = requests.get(url, params=params)
         response.raise_for_status
         api_response = response.json()
-        number_of_vacancies[language] = api_response["found"]
-    pprint(number_of_vacancies)
+        rub_salaries = [
+            i for i in [predict_rub_salary(item)
+            for item in api_response["items"]]
+            if i is not None
+        ]
+        vacancies_stats[language] = {}
+        # pprint(vacancies_stats)
+        vacancies_stats[language]["vacancies_found"] = api_response["found"]
+        vacancies_stats[language]["vacancies_processed"] = len(rub_salaries)
+        vacancies_stats[language]["average_salary"] = int(
+            sum(s for s in rub_salaries) /
+            vacancies_stats[language]["vacancies_processed"]
+            )        
+
+    pprint(vacancies_stats)
 
 
 if __name__ == "__main__":
