f618214020e382e082296a1f8409de246ec96e77
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-11-05 13:38:19 +0300

    Dry the code

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ def predict_salary(salary_from, salary_to, offer, currency):
         return None
     elif salary_from and salary_to:
         return (salary_from + salary_to) / 2
-    elif salary_from and not salary_to:
+    elif salary_from:
         return salary_from * 1.2
-    elif not salary_from and salary_to:
+    elif salary_to:
         return salary_to * 0.8
 
 
